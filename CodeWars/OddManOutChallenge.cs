3856b5fa2ba6825c187894bb2d066f153fc8c14e
Tim Bowland <tbowland@outlook.com>
2018-01-15 21:12:23 -0700

    Do odd man out challenge

---
+++ CodeWars/OddManOutChallenge.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+	public class OddManOutChallenge
+	{
+		// Array at least 3 int all are either odd or even except one item
+		// Return the one that does not belong
+		#region Mine
+
+		public int OddManOut(int[] list)
+		{
+			// look at first 2 to figure out what we are searching for
+			int item;
+			int current = 2;
+			if(list[0] % 2 != list[1] % 2)
+			{
+				// we know that one of these two are the odd one out
+				return list[0] % 2 == list[2] % 2 ? list[1] : list[0];
+			}
+			else
+			{
+				// we know that these two are not it and what the type is we are looking for
+				do
+				{
+					item = list[current++];
+				} while (item % 2 == list[0] % 2);
+			}
+			return item;
+		}
+
+		#endregion
+
+		#region Best
+
+		public int OddManOutBest(int[] integers)
+		{
+			List<int> even = integers.Where(item => item % 2 == 0).ToList();
+			List<int> odd = integers.Where(item => item % 2 != 0).ToList();
+			return even.Count() == 1 ? even.First() : odd.First();
+		}
+
+		public int OddManOutBest2(int[] list)
+		{
+			return list.GroupBy(item => item % 2).First(gb => gb.Count() == 1).First();
+		}
+
+		#endregion
+	}
+}
---
+++ UnitTests/OddManOutTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+namespace UnitTests
+{
+	/// <summary>
+	/// Summary description for OddManOutTests
+	/// </summary>
+	[TestClass]
+	public class OddManOutTests
+	{
+		OddManOutChallenge omo = new OddManOutChallenge();
+
+		[TestMethod]
+		public void OmoTest1()
+		{
+			int[] exampleTest1 = { 2, 6, 8, -10, 3 };
+			Assert.AreEqual(3, omo.OddManOut(exampleTest1));
+			Assert.AreEqual(3, omo.OddManOutBest(exampleTest1));
+			Assert.AreEqual(3, omo.OddManOutBest2(exampleTest1));
+		}
+
+		[TestMethod]
+		public void OmoTest2()
+		{
+			int[] exampleTest2 = { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 };
+			Assert.AreEqual(206847684, omo.OddManOut(exampleTest2));
+			Assert.AreEqual(206847684, omo.OddManOutBest(exampleTest2));
+			Assert.AreEqual(206847684, omo.OddManOutBest2(exampleTest2));
+		}
+
+		[TestMethod]
+		public void OmoTest3()
+		{
+			int[] exampleTest3 = { int.MaxValue, 0, 1 };
+			Assert.AreEqual(0, omo.OddManOut(exampleTest3));
+			Assert.AreEqual(0, omo.OddManOutBest(exampleTest3));
+			Assert.AreEqual(0, omo.OddManOutBest2(exampleTest3));
+		}
+
+		[TestMethod]
+		public void OmoTest4()
+		{
+			int[] exampleTest4 = { 2, 4, 0, 100, 4, 11, 2602, 36 };
+			Assert.AreEqual(11, omo.OddManOut(exampleTest4));
+			Assert.AreEqual(11, omo.OddManOutBest(exampleTest4));
+			Assert.AreEqual(11, omo.OddManOutBest2(exampleTest4));
+		}
+
+		[TestMethod]
+		public void OmoTest5()
+		{
+			int[] exampleTest5 = { 160, 3, 1719, 19, 11, 13, -21 };
+			Assert.AreEqual(160, omo.OddManOut(exampleTest5));
+			Assert.AreEqual(160, omo.OddManOutBest(exampleTest5));
+			Assert.AreEqual(160, omo.OddManOutBest2(exampleTest5));
+		}
+	}
+}
