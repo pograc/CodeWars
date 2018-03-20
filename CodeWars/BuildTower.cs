0d71533ad0d6f1f447b5b29c8fd5e9e839e322b2
Tim Bowland <tbowland@outlook.com>
2018-03-19 21:14:18 -0600

    BuildTower challenge

---
+++ CodeWars/BuildTower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+	public class BuildTower
+	{
+		public string[] TowerBuilder(int nFloors)
+		{
+			int stringLength = nFloors * 2 - 1;
+			int starCount = 1;
+			string[] returnValue = new string[nFloors];
+			for(int count = 0; count < nFloors; count++)
+			{
+				returnValue[count] = string.Concat(new string(' ', (stringLength - starCount) / 2), new string('*', starCount), new string(' ', (stringLength - starCount) / 2));
+				starCount += 2;
+			}
+			return returnValue;
+		}
+	}
+}
---
+++ UnitTests/BuildTowerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CodeWars;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class BuildTowerTests
+	{
+		BuildTower bt = new BuildTower();
+
+		[TestMethod]
+		public void BasicBuildTowerTests()
+		{
+			Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", bt.TowerBuilder(1)));
+			Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", bt.TowerBuilder(2)));
+			Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", bt.TowerBuilder(3)));
+		}
+	}
+}
